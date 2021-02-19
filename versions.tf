e68f6d4fef60546e3173b97841d49757322a770c
harshasakhare96 <53044882+harshasakhare96@users.noreply.github.com>
2021-02-20 00:35:08 +0530

    Create versions.tf

---
+++ versions.tf
@@ -0,0 +1,15 @@
+provider "random" {
+  version = "~> 2.1"
+}
+
+provider "local" {
+  version = "~> 1.2"
+}
+
+provider "null" {
+  version = "~> 2.1"
+}
+
+provider "template" {
+  version = "~> 2.1"
+}
